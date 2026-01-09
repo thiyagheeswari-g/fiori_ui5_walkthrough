a5365c1c169c2064ad7868fe8318849b757c55d7
THIYAGHEESWARI G <thiyagheeswari.se22@bitsathy.ac.in>
2026-01-09 15:06:25 +0000

    Further commit

--- webapp/index.js
+++ webapp/index.js
@@ -1,4 +1,9 @@
-sap.ui.define([],()=>{
+sap.ui.define([
+    "sap/m/Text"
+],(Text)=>{
     "use strict";
-    alert("UI5 is ready!");
+
+    new Text({
+        text:"Hello Thiya!"
+    }).placeAt("content");
 });
